27bac7aca8943d94e2c51a26d36bc3419def051a
Andrii Hordiienko <30300588+drew875@users.noreply.github.com>
2025-06-06 16:59:56 +0300

    hw1

--- OneDrive/Desktop/prots/TYPE/src/task-3.ts
+++ OneDrive/Desktop/prots/TYPE/src/task-3.ts
@@ -1,14 +1,14 @@
-const usernames: string[] = ["alice", "bob", "charlie"];
-const ratings: number[] = [4.5, 3.8, 5];
 interface Product {
-    id: number;
+    readonly id: number;
     title: string;
+    description?: string;
+
 }
 
-const product: Product[] =
-    [{ id: 1, title: "Phone" },
-    { id: 2, title: "Laptop" },
-    ]
-console.log(usernames);
-console.log(ratings);
-console.log(product);
+const product: Product = {
+    id: 1,
+    title: "Tablet",
+    description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
